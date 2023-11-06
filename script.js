b3ec2ffc0674c7f3a29ab7260a50247bbe1c5093
Pallavi Harish <106737458+PallaviHarish@users.noreply.github.com>
2023-11-06 20:45:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,81 @@
-const questions = [
-    {
-        question:"What city is known as 'The Eternal City'?",
-        answers:[
-            { text: "Adelaide", correct: false},
-            { text: "Philadelphia", correct: false},
-            { text: "Denver", correct: false},
-            { text: "Rome", correct: true},
-        ]
+// const questions = [
+//     {
+//         question:"What city is known as 'The Eternal City'?",
+//         answers:[
+//             { text: "Adelaide", correct: false},
+//             { text: "Philadelphia", correct: false},
+//             { text: "Denver", correct: false},
+//             { text: "Rome", correct: true},
+//         ]
+//     },
+//     {
+//         question:"How many faces does a Dodecahedron have?",
+//         answers:[
+//             { text: "11", correct: false},
+//             { text: "12", correct: true},
+//             { text: "13", correct: false},
+//             { text: "14", correct: false},
+//         ]
+//     },
+//     {
+//         question:"What is a group of pandas known as?",
+//         answers:[
+//             { text: "Bob", correct: false},
+//             { text: "Colony", correct: false},
+//             { text: "An embarrassment", correct: true},
+//             { text: "Shoal", correct: false},
+//         ]
+//     },
+//     {
+//         question:"Which planet has the most moons?",
+//         answers:[
+//             { text: "Saturn", correct: true},
+//             { text: "Jupiter", correct: false},
+//             { text: "Neptune", correct: false},
+//             { text: "Uranus", correct: false},
+//         ]
+//     },
+// ];
+console.log("hello")
+const a =5
+console.log(a)
+const questions = {
+    1: {
+      question: "What city is known as 'The Eternal City'?",
+      answers: ["Adelaide", "Philadelphia", "Denver", "Rome"],
+      correctAnswer: "Rome",
     },
-    {
-        question:"How many faces does a Dodecahedron have?",
-        answers:[
-            { text: "11", correct: false},
-            { text: "12", correct: true},
-            { text: "13", correct: false},
-            { text: "14", correct: false},
-        ]
+    2: {
+      question: "How many faces does a Dodecahedron have?",
+      answers: ["11", "12", "13", "14"],
+      correctAnswer: "12",
     },
-    {
-        question:"What is a group of pandas known as?",
-        answers:[
-            { text: "Bob", correct: false},
-            { text: "Colony", correct: false},
-            { text: "An embarrassment", correct: true},
-            { text: "Shoal", correct: false},
-        ]
+    3: {
+      question: "What is a group of pandas known as?",
+      answers: ["Bob", "Colony", "An embarrassment", "Shoal"],
+      correctAnswer: "An embarrassment",
     },
-    {
-        question:"Which planet has the most moons?",
-        answers:[
-            { text: "Saturn", correct: true},
-            { text: "Jupiter", correct: false},
-            { text: "Neptune", correct: false},
-            { text: "Uranus", correct: false},
-        ]
+    4: {
+      question: "Which planet has the most moons?",
+      answers: ["Saturn", "Jupiter", "Neptune", "Uranus"],
+      correctAnswer: "Saturn",
     },
-];
+    5: {
+      question: "What is the closest planet to the Sun?",
+      answers: ["Mercury", "Venus", "Earth", "Mars"],
+      correctAnswer: "Mercury",
+    },
+  };
 
 const questionElem = document.getElementById("question");
 const answerButton = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
 
-let currentQuesIndex = 0;
+let currentQuesIndex = 1;
 let score = 0;
 
 function startQuiz(){
-    currentQuesIndex = 0;
+    currentQuesIndex = 1;
     score = 0;
     nextButton.innerHTML="Next";
     showQuestion();
@@ -56,20 +86,29 @@ function showQuestion(){
     resetState();
 
     let currentQuestion = questions[currentQuesIndex];
-    let questionNo = currentQuesIndex + 1;
+    let questionNo = currentQuesIndex;
     questionElem.innerHTML = questionNo + ". " + currentQuestion.question;
 
     console.log(questionNo + ". " + currentQuestion.question);
-    currentQuestion.answers.forEach(answer =>{
+
+    // currentQuestion.answers.forEach(answer =>{
+    //     const button = document.createElement("button");
+    //     button.innerHTML = answer.text;
+    //     button.classList.add("btn");
+    //     answerButton.appendChild(button);
+    //     if(answer.correct){
+    //         button.dataset.correct = answer.correct;
+    //     }
+    //     button.addEventListener("click",checkAnswer);
+    // });
+
+    for(let index=0;index<currentQuestion.answers.length;index++){
         const button = document.createElement("button");
-        button.innerHTML = answer.text;
+        button.innerHTML = currentQuestion.answers[index];
         button.classList.add("btn");
         answerButton.appendChild(button);
-        if(answer.correct){
-            button.dataset.correct = answer.correct;
-        }
         button.addEventListener("click",checkAnswer);
-    });
+    }
 }
 
 function resetState(){
@@ -81,7 +120,9 @@ function resetState(){
 
 function checkAnswer(e){
     const selectedBtn = e.target;
-    const isCorrect = selectedBtn.dataset.correct === "true";
+    console.log(questions[currentQuesIndex].correctAnswer)
+    const isCorrect = selectedBtn.firstChild.textContent == questions[currentQuesIndex].correctAnswer;
+    console.log(isCorrect)
     if(isCorrect){
         selectedBtn.classList.add("correct");
         score++;
@@ -89,16 +130,17 @@ function checkAnswer(e){
         selectedBtn.classList.add("incorrect");
     }
     Array.from(answerButton.children).forEach(button =>{
-        if(button.dataset.correct === "true"){
-            button.classList.add("correct");
-        }
-        button.disabled = true;
-    });
+        console.log(selectedBtn.correctAnswer);
+         if(button.firstChild.textContent === questions[currentQuesIndex].correctAnswer){
+             button.classList.add("correct");
+         }
+         button.disabled = true;
+     });
     nextButton.style.display = "block";
 }
 
 nextButton.addEventListener("click",()=>{
-    if(currentQuesIndex<questions.length){
+    if(currentQuesIndex<6){
         handleNextButton();
     }else{
         startQuiz();
@@ -107,7 +149,7 @@ nextButton.addEventListener("click",()=>{
 
 function handleNextButton(){
     currentQuesIndex++;
-    if(currentQuesIndex<questions.length){
+    if(currentQuesIndex<6){
         showQuestion();
     }else{
         showScore();
@@ -116,7 +158,7 @@ function handleNextButton(){
 
 function showScore(){
     resetState();
-    questionElem.innerHTML = `You scored ${score} out of ${questions.length} !`;
+    questionElem.innerHTML = `You scored ${score} out of ${5} !`;
     nextButton.innerHTML = "Play Again";
     nextButton.style.display = "block";
 }
